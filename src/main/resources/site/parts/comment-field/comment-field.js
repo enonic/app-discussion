1bea9ff0d64202c8c477eeb1e05ce34a7cd14bb6
Persijn Kwekkeboom <pkw@enonic.com>
2019-01-22 14:31:01 +0100

    localize 6.14.3

--- src/main/resources/site/parts/comment-field/comment-field.js
+++ src/main/resources/site/parts/comment-field/comment-field.js
@@ -4,36 +4,45 @@ var thymeleaf = require("/lib/thymeleaf");
 var tools = require("/lib/tools");
 var commentLib = require("/lib/commentManager");
 var auth = require("/lib/xp/auth");
-var adminLib = require('/lib/xp/admin');
+//var adminLib = require('/lib/xp/admin');
 var i18nLib = require('/lib/xp/i18n');
+
+function createLocalizeParam(word, lang) {
+    var obj = {
+        key: word,
+    };
+    if (lang) {
+        obj.lang = lang;
+    }
+    //log.info(JSON.stringify(obj, null, 2));
+    return obj;
+}
 
 //TODO rename comment-field to something better
 exports.get = function (ref) {
     var portalContent = portal.getContent();
 
-    var content = contentLib.get({key: portalContent._id});
+    var content = contentLib.get({ key: portalContent._id });
     var langCode = content.language;
 
     //Lang code is wrongly formated (sometimes)
     langCode = langCode ? langCode.replace(/_/g, '-') : "";
 
+    //log.info(langCode);
+
     var discussion = commentLib.getComments(portalContent._id);
+    //createLocalizeParam("replyMessage", langCode)
 
     var locale = {
         reply: i18nLib.localize({
-            key: "replyMessage",
-            locale: langCode,
+            key: "replyMessage" 
         }),
-        newComment: i18nLib.localize({
-            key: "newComment",
-            locale: langCode,
-        }),
-        post: i18nLib.localize({
-            key: "post",
-            locale: langCode,
-        }),
+        newComment: i18nLib.localize(
+            createLocalizeParam("newComment", langCode)
+        ), //|| "Ny kommentar",
+        post: i18nLib.localize(createLocalizeParam("post", langCode)),
     };
-        
+
     var model = {
         discussion: discussion,
         currentContent: content._id,
@@ -43,6 +52,8 @@ exports.get = function (ref) {
         userId: auth.getUser().key,
         locale: locale,
     };
+
+    //log.info(JSON.stringify(locale, null, 2));
 
 
     model.render = true;
@@ -61,7 +72,7 @@ exports.get = function (ref) {
     // log.info(JSON.stringify(siteConfig, null, 2));
 
     if (siteConfig.defaultStyle) {
-        addition.push("<link rel='stylesheet' href='"+ portal.assetUrl({ path: "css/default.css" }) + "'/>");
+        addition.push("<link rel='stylesheet' href='" + portal.assetUrl({ path: "css/default.css" }) + "'/>");
     }
 
     return {
